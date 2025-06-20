1c6d4de8ac51014d110ae1679b913bbed9f77776
ssongha0831 <dbfk0831@gmail.com>
2025-06-20 22:47:15 +0900

    feat: implement decorator pattern and update README with theory summary

--- src/com/hayeon/jvm/JavaStudy4_02.java
+++ src/com/hayeon/jvm/JavaStudy4_02.java
@@ -12,23 +12,43 @@ import java.util.Arrays;
 }
 
 class UserService {
-
     @LogParameters
     public void login(String username, String password) {
         System.out.println("로그인 처리 로직 실행 중...");
     }
+    @LogParameters
+    public void login(String username) {
+        System.out.println("단일 인자 로그인 처리 로직 실행 중...");
+    }
+    @LogParameters
+    public void register(String username, String password, int age) {
+        System.out.println("회원가입 처리 로직 실행 중...");
+    }
 }
+
 public class JavaStudy4_02 {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         UserService userService = new UserService();
 
-        String methodName = "login";
-        Object[] params = {"ha_yeon", "1234"};
+        Object[][] testCases  = {
+                {"login", new Class[]{String.class, String.class}, new Object[]{"ha_yeon", "1234"}},
+                {"login", new Class[]{String.class}, new Object[]{"ha_yeon"}},
+                {"register", new Class[]{String.class, String.class, int.class}, new Object[]{"hy", "pw", 22}}
+        };
 
-        for (Method method : UserService.class.getDeclaredMethods()) {
-            if (method.getName().equals(methodName) && method.isAnnotationPresent(LogParameters.class)) {
-                System.out.println("[LOG] " + method.getName() + " called with parameters: " + Arrays.toString(params));
-                method.invoke(userService, params); // 실제 메서드 실행
+        for (Object[] testCase : testCases) {
+            String methodName = (String) testCase[0];
+            Class<?>[] paramTypes = (Class<?>[]) testCase[1];
+            Object[] params = (Object[]) testCase[2];
+
+            try {
+                Method method = UserService.class.getDeclaredMethod(methodName, paramTypes);
+                if (method.isAnnotationPresent(LogParameters.class)) {
+                    System.out.println("[LOG] " + method.getName() + " called with parameters: " + Arrays.toString(params));
+                    method.invoke(userService, params);
+                }
+            } catch (NoSuchMethodException e) {
+                System.out.println("해당 메서드 없음: " + methodName + Arrays.toString(paramTypes));
             }
         }
     }
